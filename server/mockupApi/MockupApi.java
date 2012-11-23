2adc1ff2bf91369f8a6bf0be1b46fe7b7820fd58
Sigfrid Lindhe <sigge.lindhe@gmail.com>
2012-11-23 21:57:39 +0100

    Skapat package för Sigges och Martins mockup-API

---
+++ server/mockupApi/MockupApi.java
@@ -0,0 +1,17 @@
+
+
+public class MockupApi {
+	 public MockupApi () {
+		 
+	 }
+	 
+	 /*
+	  * Returns a valid session id that can be stored by the client
+	  */
+	 long authenticate(String id, Strin password) {
+		 return 123;
+	 }
+	 
+	 
+	
+}

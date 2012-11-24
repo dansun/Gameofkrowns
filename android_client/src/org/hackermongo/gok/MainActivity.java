08e9aaa6fd6d00582970abf69e24baa349b0ca02
Viktor Holtenäs <viktor.holtenas@gmail.com>
2012-11-24 15:53:53 +0100

    Added view.

--- android_client/src/org/hackermongo/gok/MainActivity.java
+++ android_client/src/org/hackermongo/gok/MainActivity.java
@@ -2,13 +2,19 @@ package org.hackermongo.gok;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d("LOG_TAG", "MainActivity.onCreate()");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// AttributeSet attrs;
+		// setContentView(new GameView(this, null));
+
+	}
 
 }

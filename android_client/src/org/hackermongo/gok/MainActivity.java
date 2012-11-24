33a6cafb2b2c458b4cab3dad10668fa0d86d0083
Viktor Holtenäs <viktor.holtenas@gmail.com>
2012-11-24 18:45:46 +0100

    Random.

--- android_client/src/org/hackermongo/gok/MainActivity.java
+++ android_client/src/org/hackermongo/gok/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
 		Log.d(getClass().getSimpleName(), "MainActivity.onCreate()");
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 	}
 
--- android_client/src/org/hackermongo/gok/views/GameView.java
+++ android_client/src/org/hackermongo/gok/views/GameView.java
@@ -1,7 +1,14 @@
 package org.hackermongo.gok.views;
 
+import java.util.List;
+
+import org.hackermongo.gok.model.Game;
+import org.hackermongo.gok.model.Region;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -10,24 +17,53 @@ import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Callback {
 
+	private Game mGame;
+	private Paint mTextPaint = new Paint();
+	{
+		mTextPaint.setColor(Color.BLACK);
+	}
+
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		final SurfaceHolder holder = getHolder();
 		holder.addCallback(this);
+
+		mGame = new Game();
 	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
-		Log.d(getClass().getSimpleName(), "GameView.surfaceChanged()");
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		Log.d(getClass().getSimpleName(), "GameView.surfaceCreated()");
 		final Canvas c = holder.lockCanvas();
-		c.drawARGB(255, 255, 0, 0);
+		drawGame(c);
+
 		holder.unlockCanvasAndPost(c);
+	}
+
+	private void drawGame(Canvas c) {
+		c.drawARGB(255, 255, 0, 0);
+
+		final List<Region> regions = mGame.getRegions();
+		if (null != regions) {
+			drawRegions(c, regions);
+		}
+	}
+
+	private void drawRegions(Canvas c, List<Region> regions) {
+		final float rowPixels = c.getHeight() * 0.10f;
+		Log.d(getClass().getSimpleName(), "GameView.drawRegions() pixels: "
+				+ rowPixels);
+		c.save();
+		for (Region r : regions) {
+			c.translate(0, rowPixels);
+			c.drawText(r.getName(), 0, 0, mTextPaint);
+		}
+		c.restore();
 	}
 
 	@Override

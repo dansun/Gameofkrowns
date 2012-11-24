e549240981a039463094a1ca6953ba96966786d4
Viktor Holtenäs <viktor.holtenas@gmail.com>
2012-11-24 14:06:13 +0100

    Added new icon.

---
+++ android_client/src/org/hackermongo/gok/GameView.java
@@ -0,0 +1,14 @@
+package org.hackermongo.gok;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView {
+
+	public GameView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}

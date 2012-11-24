2f07fe791ab8a431c671fdc8c29b28c995c2bf64
siggelund <sigge.lindhe@gmail.com>
2012-11-24 16:07:40 +0100

    Bytte ut sessionId till password

--- gameofkrowns-common/src/main/java/model/MockupApi.java
+++ gameofkrowns-common/src/main/java/model/MockupApi.java
@@ -24,21 +24,21 @@ public class MockupApi {
 	 /*
 	  * Returns the match status for the player
 	  */
-	 public UserGeneralStatus getGeneralStatus(String userId, String sessionId) {
+	 public UserGeneralStatus getGeneralStatus(String userId, String password) {
 		 return new UserGeneralStatus(1,3);
 	 }
 	 
 	 /*
 	  *  Returns the games that are currently active for the user
 	  */
-	 public ArrayList<ActiveGame> getActiveGames(String userId, String sessionId) {
+	 public ArrayList<ActiveGame> getActiveGames(String userId, String password) {
 		 return new ArrayList<ActiveGame>();
 	 }
 	 
 	 /*
 	  * Returns the friends of this user. Gustaf does not have any friends. His penis is too small.
 	  */
-	 public ArrayList<Friend> getFriends(String userId, String sessionId) {
+	 public ArrayList<Friend> getFriends(String userId, String password) {
 		 return new ArrayList<Friend>();
 	 }
 	 
@@ -46,7 +46,7 @@ public class MockupApi {
 	  * Requests the ID for a game to be created. If allowRandoms is true, the server will try to fill out to four players
 	  * with random players looking for games
 	  */
-	 public long createGame(String userId, long sessionId, boolean allowRandoms, ArrayList<String> userids) {
+	 public long createGame(String userId, long password, boolean allowRandoms, ArrayList<String> userids) {
 		 return 0;
 	 }
 	 
@@ -60,21 +60,21 @@ public class MockupApi {
 	 /*
 	  * Gets the invitations that the user is currently involved in
 	  */
-	 public ArrayList<Invitation> getInvitations(String userId, String sessionId) {
+	 public ArrayList<Invitation> getInvitations(String userId, String password) {
 		 return new ArrayList<Invitation>();
 	 }
 	 
 	 /*
 	  * Gets the history for the specified turn
 	  */
-	 public ArrayList<HistoryEvent> getHistory(String userId, String sessionId,long gameId,long turnIndex) {
+	 public ArrayList<HistoryEvent> getHistory(String userId, String password,long gameId,long turnIndex) {
 		 return new ArrayList<HistoryEvent>();
 	 }
 	 
 	 /*
 	  * Performs the action for the user. If action is not allowed, unchanged game obj is returned
 	  */
-	 public Game performAction(String userId,String sessionId,String gameId,String regionId,String actionId) {
+	 public Game performAction(String userId,String password,String gameId,String regionId,String actionId) {
 		 return new Game();
 	 }
 	
